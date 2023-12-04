7ccddce0e592f56121e1f18008caf537aeb0087b
Ana Carolina Schmitz da Silva <101817785+anacschmitz@users.noreply.github.com>
2023-12-03 21:16:34 -0300

    Atualizando regras de negócio.

--- Biblio_Tech2/BiblioTech/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BibliotechConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BiblioTech'
--- Biblio_Tech2/BiblioTech/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('home/', views.index, name='home'),
-  # path('add-student/', views.add_estudante, name='add-student'),
-  ]
